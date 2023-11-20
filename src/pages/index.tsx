888dc5094c293dbef8cc5790b37aedb62ce631e9
Ackarmen <lyaforis@gmail.com>
2023-11-20 17:08:42 +0300

    setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
 import { Seo } from "@/ui/components/seo";
-// import Head from "next/head";
+import { Typography } from "@/ui/design-system/typography/typography";
 
 export default function Home() {
 	return (
 		<>
 			<Seo title="Code Collab" description="description..." />
-			<h1>Bienvenue sur Code Collab, la platforme collaborative!</h1>
+
+			<div>
+				<Typography variant="display" component="h1">
+					Code Collab
+				</Typography>
+				<Typography theme="primary" variant="h2" component="h2">
+					Code Collab
+				</Typography>
+				<Typography variant="display" component="h1">
+					Code Collab
+				</Typography>
+				<Typography variant="display" component="h1">
+					Code Collab
+				</Typography>
+			</div>
 		</>
 	);
 }
